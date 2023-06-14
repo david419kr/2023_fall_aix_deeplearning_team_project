ec5ec3bdce24c5096309b7c3e952b995b6b106bf
david419kr <david419kr@gmail.com>
2023-06-14 19:21:17 +0900

    updated new code from kaggle

--- project_codes/test.py
+++ project_codes/test.py
@@ -27,7 +27,7 @@ def get_parser():
                         help='default(cuda)')
     
     #dataloader
-    parser.add_argument('--data', default='/home/tnt/Downloads/NIH_images',
+    parser.add_argument('--data', default='../images',
                         help='Data root ')
     parser.add_argument('--batch_size', default=5, type=int,
                         help='batch size per gpu')
